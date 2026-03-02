2b7e953a2c01268fc1a2981314ab8575cb4acc46
Rajeshwari Ligade <ligade.rajeshwari@gmail.com>
2026-03-02 10:57:23 +0000

    Trigger pipeline version 2

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello DevOps CI/CD on GCP!"
+    return "CI/CD Pipeline Successfully Triggered - Version 2!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080)
